e14c4f441a68f8e49047c6b1152b0ee0e2f68dd5
kaze-wind-dev <kaze.dev.wind@gmail.com>
2026-02-28 18:01:04 +0900

    Initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const SITE_URL = '';
+export const SITE_NAME = 'Kaze Astro Play Ground';
+export const SITE_DESCRIPTION =
+  'Kaze Astro Play GroundはAstroのPlay Groundです。Astroの技術や実装ケースを試すためのサイトです。';
+export const SITE_KEYWORDS =
+  'Astro, Play Ground, アストロ, プレイグラウンド, Kaze Astro Play Ground';
+export const SITE_IMAGE = `${SITE_URL}/ogp_image.jpg`;
