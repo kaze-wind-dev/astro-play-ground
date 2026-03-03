5ff7ae0c195770534ae621ec62e73aa130722416
kaze-wind-dev <kaze.dev.wind@gmail.com>
2026-03-04 00:39:15 +0900

    update:

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,6 +8,10 @@ export const SITE_IMAGE_PATH = '/ogp_image.jpg';
 
 export const NAVIGATION: Navigation[] = [
   {
+    href: '/semantic-a11y',
+    label: 'HTMLセマンティクス検証（ケース1〜3）',
+  },
+  {
     href: '/dialog-menu',
     label: 'ダイアログメニュー',
   },
