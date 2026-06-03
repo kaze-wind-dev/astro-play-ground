56430ab2169bc9af869bb4356877f435a78b8818
kaze-wind-dev <kaze.dev.wind@gmail.com>
2026-06-03 09:47:36 +0900

    feat

--- src/pages/animation/navigation.ts
+++ src/pages/animation/navigation.ts
@@ -9,4 +9,20 @@ export const NAVIGATION: Navigation[] = [
     href: 'header-animation/change-elements-size',
     label: 'スクロール後に要素の大きさ変化を与えるアニメーション',
   },
+  {
+    href: 'inertial-scroll/lenis',
+    label: '慣性スクロール(Lenis)'
+  },
+  {
+    href: 'inertial-scroll/js',
+    label: '慣性スクロール(js)'
+  },
+  {
+    href: 'parallax-animation/lenis',
+    label: 'パララックス・スクロール（Lenis）',
+  },
+  {
+    href: 'parallax-animation/js',
+    label: 'パララックス・スクロール（js）',
+  },
 ];
