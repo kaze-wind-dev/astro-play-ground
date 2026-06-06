9a181a879c9692ffba170928e8daaf6d50956940
kaze-wind-dev <kaze.dev.wind@gmail.com>
2026-06-06 11:10:16 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const repository = process.env.GITHUB_REPOSITORY;
-const [owner, repo] = repository ? repository.split('/') : [];
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-const site =
-  process.env.SITE_URL ||
-  (owner ? 'https://kaze-wind-dev.github.io' : 'http://localhost:4321');
-const base =
-  process.env.BASE_PATH || (isGitHubActions && repo ? `/${repo}` : '/');
+// const repository = process.env.GITHUB_REPOSITORY;
+// const [owner, repo] = repository ? repository.split('/') : [];
+// const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+// const site = process.env.SITE_URL || 'http://localhost:4321';
+// const base =
+//   process.env.BASE_PATH || (isGitHubActions && repo ? `/${repo}` : '/');
 
 // https://astro.build/config
 export default defineConfig({
-  site,
-  base,
+  // site,
+  // base,
   integrations: [sitemap()],
 });
